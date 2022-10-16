899f341b0c85783bf6fcc86063755e3e00374d1e
Vitor Azevedo <vitor.azevedo@icloud.com>
2022-10-16 15:05:04 +0100

    finished footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  navs = [
+    { title: 'Quick Links', buttons: ['Homepage', 'Roaming Information', 'Service Fees', 'Report Fraud', 'Device Repair'] },
+    { title: 'Offers', buttons: ['Mobile Plans', 'Prepaid SIM', 'Internet at Home', '5G Speed Option', 'Saving options', 'Wi-Fi Calling'] },
+    { title: 'My yallo', buttons: ['Account Login', 'My Products', 'Usage History', 'Past Invoices', 'Settings'] },
+    { title: 'About yallo', buttons: ['Company', 'Media Center', 'Review & Awards', 'Business Customers', 'Reseller Login'] },
+  ];
 }
